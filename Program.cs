a838564de252105ad273954fb2961b876c141808
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-06 11:15:44 +0530

    UC2

--- Program.cs
+++ Program.cs
@@ -9,15 +9,18 @@ namespace UserRegistrationProblem
         {
             Console.WriteLine("Welcome to user registration problem");
             Console.WriteLine("Enter the first name ");
+            Console.WriteLine("Enter the lat name ");
             string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
             string regexPattern = "[A-Z][a-z]{2,}";
         
             Regex regex = new Regex(regexPattern);
            
-            if (regex.IsMatch(firstName))
+            if (regex.IsMatch(firstName) && regex.IsMatch(lastName))
                 Console.WriteLine("Name entered is valid");
             else
                 Console.WriteLine("Name entered is invalid");
+
         }
     }
 }
