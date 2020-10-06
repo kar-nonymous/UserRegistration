4ddf117a5713a277b530a0868945563765cb56a7
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-06 11:59:23 +0530

    UC4

--- Program.cs
+++ Program.cs
@@ -14,12 +14,17 @@ namespace UserRegistrationProblem
             string lastName = Console.ReadLine();
             Console.WriteLine("Enter the email id");
             string email = Console.ReadLine();
+            Console.WriteLine("Enter the mobile number");
+            string mobileNum = Console.ReadLine();
+
             string nameRegexPattern = "[A-Z][a-z]{2,}";
             string emailRegexPattern = "^[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
-
+            string mobileNumRegexPattern = "^[0-9]{2}[ ][0-9]{10}$";
 
             Regex nameRegex = new Regex(nameRegexPattern);
             Regex emailRegex = new Regex(emailRegexPattern);
+            Regex mobileNumRegex = new Regex(mobileNumRegexPattern);
+
             if (nameRegex.IsMatch(firstName) && nameRegex.IsMatch(lastName))
                 Console.WriteLine("Name entered is valid");
             else
@@ -28,6 +33,10 @@ namespace UserRegistrationProblem
                 Console.WriteLine("The entered email is valid");
             else
                 Console.WriteLine("The entered email is invalid");
+            if (mobileNumRegex.IsMatch(mobileNum))
+                Console.WriteLine("The entered mobile number is valid");
+            else
+                Console.WriteLine("The entered mobile number is invalid");
 
         }
     }
