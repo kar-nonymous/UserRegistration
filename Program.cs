1f4366c51ce0838c7d2787492e2220158eb1379a
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-06 11:48:52 +0530

    UC3

--- Program.cs
+++ Program.cs
@@ -9,17 +9,25 @@ namespace UserRegistrationProblem
         {
             Console.WriteLine("Welcome to user registration problem");
             Console.WriteLine("Enter the first name ");
-            Console.WriteLine("Enter the lat name ");
             string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the lat name ");
             string lastName = Console.ReadLine();
-            string regexPattern = "[A-Z][a-z]{2,}";
-        
-            Regex regex = new Regex(regexPattern);
-           
-            if (regex.IsMatch(firstName) && regex.IsMatch(lastName))
+            Console.WriteLine("Enter the email id");
+            string email = Console.ReadLine();
+            string nameRegexPattern = "[A-Z][a-z]{2,}";
+            string emailRegexPattern = "^[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
+
+
+            Regex nameRegex = new Regex(nameRegexPattern);
+            Regex emailRegex = new Regex(emailRegexPattern);
+            if (nameRegex.IsMatch(firstName) && nameRegex.IsMatch(lastName))
                 Console.WriteLine("Name entered is valid");
             else
                 Console.WriteLine("Name entered is invalid");
+            if (emailRegex.IsMatch(email))
+                Console.WriteLine("The entered email is valid");
+            else
+                Console.WriteLine("The entered email is invalid");
 
         }
     }
