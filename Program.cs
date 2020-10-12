a4aeb1bd44f516546d425dcf760fc3e84af0ae02
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-12 13:06:55 +0530

    [Kartikeya] UC added to validate user using MS Test

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistrationProblem
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -26,45 +26,42 @@ namespace UserRegistrationProblem
                 password += key.KeyChar;
             }
             Console.WriteLine(password);
-
-            string nameRegexPattern = "[A-Z][a-z]{2,}";
-            string emailRegexPattern = "^[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
-            string mobileNumRegexPattern = "^[0-9]{2}[ ][0-9]{10}$";
-            string passwordRegexPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
-
-            Regex nameRegex = new Regex(nameRegexPattern);
-            Regex emailRegex = new Regex(emailRegexPattern);
-            Regex mobileNumRegex = new Regex(mobileNumRegexPattern);
-            Regex passwordRegex = new Regex(passwordRegexPattern);
-
-            if (nameRegex.IsMatch(firstName) && nameRegex.IsMatch(lastName))
-                Console.WriteLine("Name entered is valid");
-            else
-                Console.WriteLine("Name entered is invalid");
-            if (emailRegex.IsMatch(email))
-                Console.WriteLine("The entered email is valid");
-            else
-                Console.WriteLine("The entered email is invalid");
-            if (mobileNumRegex.IsMatch(mobileNum))
-                Console.WriteLine("The entered mobile number is valid");
-            else
-                Console.WriteLine("The entered mobile number is invalid");
-            if (passwordRegex.IsMatch(password))
-                Console.WriteLine("The password is valid");
-            else
-                Console.WriteLine("The password is invalid");
-
-            string patternOfEmail = "^[a-zA-Z0-9]+([.+_-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})?$";
-            string[] sampleEmail = {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au",
-                                    "abc@1.com","abc@gmail.com.com","abc+100@gmail.com","Abc","abc@.com.my" ,"abc123@gmail.1a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
-            Console.WriteLine("The valid sample emails are :");
-            foreach (string sample in sampleEmail)
+        }
+            public static bool ValidateFirstName(string firstName)
             {
-                if (Regex.IsMatch(sample, patternOfEmail))
-                    Console.WriteLine(sample);
-
+                string nameRegexPattern = "[A-Z][a-z]{2,}";
+                Regex nameRegex = new Regex(nameRegexPattern);
+                if (nameRegex.IsMatch(firstName) && nameRegex.IsMatch("Kartikeya"))
+                    return true;
+                else
+                return false;       
             }
-
+        public static bool ValidateEmail(string email)
+        {
+            string emailRegexPattern = "^[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
+            Regex emailRegex = new Regex(emailRegexPattern);
+            if (emailRegex.IsMatch("kartikeya.kumar@capgemini.com"))
+                return true;
+            else
+                return false;
+        }
+        public static bool ValidateMobileNum(string email)
+        {
+            string mobileNumRegexPattern = "^[0-9]{2}[ ][0-9]{10}$";
+            Regex mobileNumRegex = new Regex(mobileNumRegexPattern);
+            if (mobileNumRegex.IsMatch("91 7206183244"))
+                return true;
+            else
+                return false;
+        }
+        public static bool ValidatePassword(string email)
+        {
+            string passwordRegexPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
+            Regex passwordRegex = new Regex(passwordRegexPattern);
+            if (passwordRegex.IsMatch("Kartikeya@19"))
+                return true;
+            else
+                return false;
         }
     }
 }
